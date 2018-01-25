08680f374d3b3b8e59e2e97bb62c043862efc0ca
Xiao Ling <lingxiao@seas.upenn.edu>
2018-01-24 20:19:07 -0500

    xiao: rm

--- applications/home-page/dist/routes/index.js
+++ applications/home-page/dist/routes/index.js
@@ -2,16 +2,12 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const path = require("path");
-const html_rel_dir = "../../public/bootstrap/Homepage/";
 const index = express_1.Router();
 index.get('/candidate-0', (req, res, next) => {
+    const html_rel_dir = "../../public/bootstrap/Homepage/";
     res.sendFile(path.join(__dirname, html_rel_dir, 'candidate-0.html'));
 });
 index.get('/candidate-1', (req, res, next) => {
-    var path_to_html = path.join(__dirname, html_rel_dir, 'candidate-1.html');
-    res.sendFile(path_to_html);
-});
-index.get('/pug', (req, res, next) => {
     res.render('candidate-1', {});
 });
 exports.default = index;
--- applications/home-page/src/routes/index.ts
+++ applications/home-page/src/routes/index.ts
@@ -9,10 +9,6 @@ import * as path    from 'path'   ;
 import * as pug     from 'pug'    ;
 
 
-/**
-	@TODO: remove relative path specification
-*/
-const html_rel_dir: string = "../../public/bootstrap/Homepage/"
 const index: Router = Router();
 
 
@@ -20,9 +16,8 @@ const index: Router = Router();
 	Raw bootstrap template 
 */
 index.get('/candidate-0', (req, res, next) => {
-
+	const html_rel_dir: string = "../../public/bootstrap/Homepage/"
 	res.sendFile( path.join(__dirname, html_rel_dir, 'candidate-0.html') );
-
 });
 
 
@@ -30,17 +25,9 @@ index.get('/candidate-0', (req, res, next) => {
 	Template under development
 */
 index.get('/candidate-1', (req, res, next) => { 
-
-	var path_to_html = path.join(__dirname, html_rel_dir, 'candidate-1.html');
-	res.sendFile( path_to_html );
-
+	res.render('candidate-1', {});
 });
 
-index.get('/pug', (req, res, next) => {
-
-	res.render('candidate-1', {});
-
-})
 
 export default index;
 
